ee30209d770b011aeecd1bc450dd69db00197c87
Parth Singh <65109271+Parth-AI@users.noreply.github.com>
2020-05-18 18:32:15 +0530

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-# pro-23
+# SupplyMission
---
+++ box.js
@@ -0,0 +1,44 @@
+class Box{
+     constructor(x, y, width, height){
+
+          var options = {
+               isStatic : true
+          }
+
+          this.body = Bodies.rectangle(x,y,width,height,options);
+          this.width = width;
+          this.height = height;
+
+          World.add(world,this.body);
+     }
+     display(){
+          var pos = this.body.position;
+          fill("red");
+          rect(pos.x,pos.y,this.width,this.height);
+     }
+}
+/*class Box {
+     constructor(x, y, width, height) {
+       var options = {
+           'restitution':0.8,
+           'friction':0.4,
+           'density':1.0
+       }
+       this.body = Bodies.rectangle(x, y, width, height, options);
+       this.width = width;
+       this.height = height;
+       
+       World.add(world, this.body);
+     }
+     display(){
+       var pos =this.body.position;
+       var angle = this.body.angle;
+       push();
+       translate(pos.x,pos.y);
+       rotate(angle);
+       rectMode(CENTER);
+       fill(255);
+       rect(0,0, this.width, this.height);
+       pop();
+     }
+   };*/
